84992de3677dcd0799b9831f5c106793e75b85b5
Tim Dysinger <tim@dysinger.net>
2008-10-17 08:34:35 -1000

    mostly complete postfix module

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -1,18 +1,38 @@
 class postfix {
   portage::use { "postfix": category => "mail-mta", use => "sasl" }
-  package { "postfix": category => "mail-mta", before => Service["postfix"] }
-  exec { "postfix-reload":
-    command => "/usr/sbin/postfix reload", refreshonly => true
+  package { "postfix":
+    category => "mail-mta", before => Exec["postfix-aliases"]
   }
   exec { "postfix-aliases":
     command => "/usr/bin/newaliases",
-    refreshonly => true,
-    notify => Exec["postfix-reload"]
-  }
-  exec { "postmap-virtual":
-    command => "/usr/sbin/postmap /etc/postfix/virtual",
-    refreshonly => true,
-    notify => Exec["postfix-reload"]
+    creates => "/etc/mail/aliases.db",
+    before => Service["postfix"]
   }
   service { "postfix": enable => true, ensure => running }
+  package { "pflogsumm": category => "net-mail" }
+  cron { "pflogsumm-report":
+    command => "/usr/bin/pflogsumm.pl -d yesterday /var/log/messages",
+    hour => 0,
+    minute => 3
+  }
+  exec { "postfix-reload":
+    command => "/usr/sbin/postfix reload", refreshonly => true
+  }
+  define setup($admin, $vhost = []) {
+    include postfix
+    file { "/etc/mail/aliases": content => template("postfix/aliases.erb") }
+    file { "/etc/postfix/virtual": content => template("postfix/virtual.erb") }
+    exec { "postmap-virtual":
+      command => "/usr/sbin/postmap /etc/postfix/virtual",
+      subscribe => File["/etc/postfix/virtual"],
+      refreshonly => true,
+      require => Package["postfix"],
+      notify => Exec["postfix-reload"]
+    }
+    file { "/etc/postfix/main.cf":
+      content => template("postfix/main.cf.erb"),
+      require => Package["postfix"],
+      notify => Exec["postfix-reload"]
+    }
+  }
 }
